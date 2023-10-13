11a95aa5cd6b06cbe87cb4953a946ee2945df28c
marchese89 <marchese89@hotmail.com>
2023-10-13 12:55:57 +0200

    si va avanti...

--- all-product.js
+++ all-product.js
@@ -1,23 +1,22 @@
 const authorization =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI4ZjE2ZDEzOWM0MzAwMTg4MTQ1NjgiLCJpYXQiOjE2OTcxODIwNjEsImV4cCI6MTY5ODM5MTY2MX0.ohTdDE9qrFCEwPMsmp9juMcJHoKfRPFeZjxYTsNGimE";
 
+const URL_API = "https://striveschool-api.herokuapp.com/api/product/";
+
 async function allProduct() {
-  const res = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: authorization,
-      },
-    }
-  );
+  const res = await fetch(URL_API, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authorization,
+    },
+  });
+  const spinner = document.getElementById("spinner");
+  if (spinner != null) {
+    spinner.classList.add("d-none");
+  }
   if (res.ok) {
     const data = await res.json();
-    const spinner = document.getElementById("spinner");
-    if (spinner != null) {
-      spinner.classList.add("d-none");
-    }
-    // console.log(data);
+    console.log(data);
     showAllProducts(data);
   } else {
     alert("Si è verificato un errore nel caricamento");
@@ -37,28 +36,30 @@ function showAllProducts(data) {
     col.classList.add("col", "col-12", "col-md-6", "col-lg-4", "col-xl-3");
 
     if (mod === "true") {
-      col.innerHTML += `<div class="card" >
+      col.innerHTML += `<div class="card d-flex flex-column" >
       <img src="${prod.imageUrl}" class="card-img-top" alt="...">
-      <div class="card-body">
+      <div class="card-body d-flex flex-column">
         <h4 class="card-title">${prod.name}</h4>
         <p class="card-text"><strong>Descrizione</strong></p>
         <p class="card-text">${prod.description}</p>
         <p class="card-text">Marca: ${prod.brand}</p>
         <p class="card-text">Prezzo: ${prod.price} <strong>&euro;</strong></p>
-        <a href="back-office.html?id=${prod._id}" class="btn btn-warning">Modifica</a>
+        <a href="back-office.html?id=${prod._id}" class="btn btn-warning mb-2">Modifica</a>
         <a href="#" class="btn btn-danger" onclick="deleteProduct('${prod._id}')">Elimina</a>
         </div>
         </div>`;
     } else {
       col.innerHTML = `
-        <div class="card" >
+        <div class="card d-flex flex-column" >
   <img src="${prod.imageUrl}" class="card-img-top" alt="...">
-  <div class="card-body">
+  <div class="card-body d-flex flex-column">
     <h4 class="card-title">${prod.name}</h4>
     <p class="card-text"><strong>Descrizione</strong></p>
     <p class="card-text">${prod.description}</p>
     <p class="card-text">Marca: ${prod.brand}</p>
     <p class="card-text">Prezzo: ${prod.price} <strong>&euro;</strong></p>
+    <a href="back-office.html?id=${prod._id}" class="btn btn-warning mb-2">Modifica</a>
+    <a href="detail.html?id=${prod._id}&view=ok" class="btn btn-info">Scopri di più</a>
     </div>
         </div>`;
     }
@@ -68,20 +69,20 @@ function showAllProducts(data) {
 }
 
 async function deleteProduct(id) {
-  const res = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/" + id,
-    {
+  const confirm = window.confirm("Sicuro di voler ELIMINARE?");
+  if (confirm) {
+    const res = await fetch(URL_API + id, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: authorization,
       },
+    });
+    if (res.ok) {
+      alert("Prodotto Eliminato Correttamente");
+      allProduct();
+    } else {
+      alert("Errori nell'eliminazione del prodotto");
     }
-  );
-  if (res.ok) {
-    alert("Prodotto Eliminato Correttamente");
-    allProduct();
-  } else {
-    alert("Errori nell'eliminazione del prodotto");
   }
 }
--- back-office.js
+++ back-office.js
@@ -1,27 +1,47 @@
 const authorization =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI4ZjE2ZDEzOWM0MzAwMTg4MTQ1NjgiLCJpYXQiOjE2OTcxODIwNjEsImV4cCI6MTY5ODM5MTY2MX0.ohTdDE9qrFCEwPMsmp9juMcJHoKfRPFeZjxYTsNGimE";
 
+const URL_API = "https://striveschool-api.herokuapp.com/api/product/";
+
 const addressBarContent = new URLSearchParams(location.search);
 const id = addressBarContent.get("id");
+const view = addressBarContent.get("view");
+
+if (view != null) {
+  const inputs = document.forms["create-product"].getElementsByTagName("input");
+  const inputsArray = Array.from(inputs);
+  inputsArray.forEach((input) => {
+    input.readOnly = true;
+  });
+}
+
 if (id != null) {
   fillProduct(id);
-  document.getElementById("form-btn").innerHTML = `
+  if (view == null) {
+    document.getElementById("form-btn").innerHTML = `
   <button class="btn btn-warning">Modifica</button>
   `;
+  } else {
+    document.getElementById("form-btn").innerHTML = "";
+    document.getElementById("reset").parentElement.innerHTML = "";
+    document.getElementsByTagName("h1")[0].innerText = "Dettagli prodotto";
+    document.getElementsByTagName("h3")[0].remove();
+  }
 }
 
 async function fillProduct() {
-  const res = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/" + id,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: authorization,
-      },
-    }
-  );
+  const res = await fetch(URL_API + id, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authorization,
+    },
+  });
   if (res.ok) {
     const prod = await res.json();
+    const immagine = document.getElementById("img");
+    if (immagine != null) {
+      immagine.setAttribute("src", prod.imageUrl);
+    }
     const inputs =
       document.forms["create-product"].getElementsByTagName("input");
     const inputsArray = Array.from(inputs);
@@ -33,34 +53,44 @@ async function fillProduct() {
   }
 }
 
+const regex = /[0-9]{1,}([.,]{1}[0-9]{2}){0,1}/;
+
 const createForm = document.getElementById("create-product");
 createForm.addEventListener("submit", (e) => {
+  let toSubmit = true;
   e.preventDefault();
   const inputs = document.forms["create-product"].getElementsByTagName("input");
   const product = {};
   const inputsArray = Array.from(inputs);
   inputsArray.forEach((input) => {
     product[input.id] = input.value;
+    if (input.id === "price" && !regex.test(input.value)) {
+      input.classList.add("bg-danger");
+      toSubmit = false;
+    } else {
+      if (input.classList.contains("bg-danger")) {
+        input.classList.remove("bg-danger");
+      }
+    }
   });
-  if (id == null) {
-    createProduct(product);
-  } else {
-    changeProduct(product);
+  if (toSubmit) {
+    if (id == null) {
+      createProduct(product);
+    } else {
+      changeProduct(product);
+    }
   }
 });
 
 async function changeProduct(product) {
-  const res = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/" + id,
-    {
-      method: "PUT",
-      body: JSON.stringify(product),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: authorization,
-      },
-    }
-  );
+  const res = await fetch(URL_API + id, {
+    method: "PUT",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authorization,
+    },
+  });
   if (res.ok) {
     alert("Prodotto Modificato Correttamente");
     fillProduct();
@@ -70,17 +100,14 @@ async function changeProduct(product) {
 }
 
 async function createProduct(product) {
-  const res = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/",
-    {
-      method: "POST",
-      body: JSON.stringify(product),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: authorization,
-      },
-    }
-  );
+  const res = await fetch(URL_API, {
+    method: "POST",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authorization,
+    },
+  });
   if (res.ok) {
     alert("prodotto creato correttamente");
   } else {
@@ -99,4 +126,9 @@ function formReset() {
 }
 
 const reset = document.getElementById("reset");
-reset.addEventListener("click", formReset);
+reset.addEventListener("click", () => {
+  const confirm = window.confirm("Sicuro di voler fare il RESET del form?");
+  if (confirm) {
+    formReset();
+  }
+});
